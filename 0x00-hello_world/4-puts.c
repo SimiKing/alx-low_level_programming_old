edd2f1fa02824f4ee48c3010dce7847362740f52
Simi King <simiking@yahoo.com>
2023-09-08 11:32:37 -0700

    C is Fun!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry point
+ * and return will always be 0(succes)
+ *
+ */
+int main(void)
+{
+	/**
+	 * Outputs the text
+	 *
+	 */
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
